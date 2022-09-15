cd1a5f3cb28bea363e3a1f28037ca0a99f41f49d
Letícia Rodrigues <leticia_rodriguesdesousa@hotmail.com>
2022-09-15 19:25:08 -0300

    Hello World

---
+++ hareexpress/src/main/java/com/project/hareexpress/HareexpressApplication.java
@@ -0,0 +1,13 @@
+package com.project.hareexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HareexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HareexpressApplication.class, args);
+	}
+
+}
