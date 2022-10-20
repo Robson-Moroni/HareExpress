4ddf5f3c05d4e9a9821d10330302b9dc5cf83fb7
Letícia Rodrigues <leticia_rodriguesdesousa@hotmail.com>
2022-10-20 12:00:28 -0300

    Telas de Cadastro do Entregador incluídas

--- hareexpress/src/main/java/com/project/hareexpress/application/controllers/IndexController.java
+++ hareexpress/src/main/java/com/project/hareexpress/application/controllers/IndexController.java
@@ -21,6 +21,26 @@ public class IndexController {
         return "cadastro_escolha_cliente_entregador";
     }
 
+    @RequestMapping("/cadastrar_entregador")
+    public String cadastrarEntregador(){
+        return "cadastro_entregador";
+    }
+
+    @RequestMapping("/cadastrar_entregador_endereco")
+    public String cadastrarEntregadorEndereco(){
+        return "cadastro_entregador_endereco";
+    }
+
+    @RequestMapping("/cadastrar_entregador_veiculo")
+    public String cadastrarEntregadorVeiculo(){
+        return "cadastro_entregador_veiculo";
+    }
+
+    @RequestMapping("/entregador_finalizado")
+    public String entregadorFinalizado(){
+        return "entregador_finalizado";
+    }
+
 
 
 
