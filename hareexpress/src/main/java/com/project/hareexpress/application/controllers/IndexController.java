111e8646e3783aba90cd4a44cc4f97c7917dd87e
Letícia Rodrigues <leticia_rodriguesdesousa@hotmail.com>
2022-10-20 19:42:59 -0300

    Alteração application.properties e mvc

--- hareexpress/src/main/java/com/project/hareexpress/application/controllers/IndexController.java
+++ hareexpress/src/main/java/com/project/hareexpress/application/controllers/IndexController.java
@@ -20,12 +20,11 @@ public class IndexController {
     public PessoaServiceImpl pessoaService;
     @Autowired
     public IUserService userService;
-
-
-
+    
     @RequestMapping("/")
-    public String index(){
-        return "index";
+    public ModelAndView ModelAndView(){
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
     }
 
     @RequestMapping("/login")
@@ -38,62 +37,69 @@ public class IndexController {
     @RequestMapping("/cadastrar")
     public ModelAndView cadastrar(){
         ModelAndView mv = new ModelAndView("cadastro_escolha_cliente_entregador");
-        mv.addObject("pessoa", pessoaService.listPessoas().get(0));
         return mv;
     }
 
     @RequestMapping("/cadastrar_entregador")
-    public String cadastrarEntregador(){
-        return "cadastro_entregador";
+    public ModelAndView cadastrarEntregador(){
+        ModelAndView mv = new ModelAndView("cadastro_entregador");
+        return mv;
     }
 
     @RequestMapping("/cadastrar_entregador_endereco")
-    public String cadastrarEntregadorEndereco(){
-        return "cadastro_entregador_endereco";
+    public ModelAndView cadastrarEntregadorEndereco(){
+        ModelAndView mv = new ModelAndView("cadastro_entregador_endereco");
+        return mv;
     }
 
     @RequestMapping("/cadastrar_entregador_veiculo")
-    public String cadastrarEntregadorVeiculo(){
-        return "cadastro_entregador_veiculo";
+    public ModelAndView cadastrarEntregadorVeiculo(){
+        ModelAndView mv = new ModelAndView("cadastro_entregador_veiculo");
+        return mv;
     }
 
     @RequestMapping("/entregador_finalizado")
-    public String entregadorFinalizado(){
-        return "entregador_finalizado";
+    public ModelAndView entregadorFinalizado(){
+        ModelAndView mv = new ModelAndView("entregador_finalizado");
+        return mv;
     }
 
     @RequestMapping("/cadastrar_escolha_fisico_juridico")
-    public String cadastrarPessoaFisicaJuridica(){
-        return "cadastro_escolha_fisico_juridico";
+    public ModelAndView cadastrarPessoaFisicaJuridica(){
+        ModelAndView mv = new ModelAndView("cadastro_escolha_fisico_juridico");
+        return mv;
     }
 
     @RequestMapping("/cadastrar_pessoa_fisica")
-    public String cadastrarPessoaFisica(){
-        return "cadastro_pessoa_fisica";
+    public ModelAndView cadastrarPessoaFisica(){
+        ModelAndView mv = new ModelAndView("cadastro_pessoa_fisica");
+        return mv;
     }
 
     @RequestMapping("/cadastrar_pessoa_fisica_endereco")
-    public String cadastrarPessoaFisicaEndereco(){
-        return "cadastro_pessoa_fisica_endereco";
+    public ModelAndView cadastrarPessoaFisicaEndereco(){
+        ModelAndView mv = new ModelAndView("cadastro_pessoa_fisica_endereco");
+        return mv;
     }
 
     
     @RequestMapping("/usuario_finalizado")
-    public String usuarioFinalizado(){
-        return "usuario_finalizado";
+    public ModelAndView usuarioFinalizado(){
+        ModelAndView mv = new ModelAndView("usuario_finalizado");
+        return mv;
     }
 
 
     @RequestMapping("/cadastrar_pessoa_juridica")
-    public String cadastrarPessoaJuridica(){
-        return "cadastro_pessoa_juridica";
+    public ModelAndView cadastrarPessoaJuridica(){
+        ModelAndView mv = new ModelAndView("cadastro_pessoa_juridica");
+        return mv;
     }
 
     @RequestMapping("/cadastrar_pessoa_juridica_endereco")
-    public String cadastrarPessoaJuridicaEndereco(){
-        return "cadastro_pessoa_juridica_endereco";
+    public ModelAndView cadastrarPessoaJuridicaEndereco(){
+        ModelAndView mv = new ModelAndView("cadastro_pessoa_juridica_endereco");
+        return mv;
     }
-
-
 
 }
